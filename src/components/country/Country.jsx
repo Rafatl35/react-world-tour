8e994c6f3afe91bb3850c076e4b2722e751a53de
Rafatl35 <rafatrahmano4@gmail.com>
2024-08-28 15:28:48 +0600

    Turn off prop types eslint warning

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,11 +1,16 @@
 import './country.css'
-const Country = ({country}) => {
+const Country = ({ country }) => {
     console.log(country)
-    const {name, flags} = country;
+    const { name, flags, population, region
+    } = country;
     return (
         <div className='country'>
-            <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
+            <h3>Name: {name?.common}</h3>
+            <p>Population: {population}</p>
+            <p>Region
+                : {region
+                }</p>
         </div>
     );
 };
